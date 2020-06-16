4b19e3c34ef5f483e43d83a176329d93a9343dde
nickdulchin <nick.dulchin@gmail.com>
2020-06-16 19:16:47 -0400

    switched to adjusted_equity_previous close and switched from nyt to cdc

--- percentages.py
+++ percentages.py
@@ -13,7 +13,12 @@ total_num = latest['confirmed']
 #covid nyt
 page = requests.get("https://www.nytimes.com/")
 soup = BeautifulSoup(page.content, 'html.parser')
-new = soup.find('h4',{"class":"svelte-pnqaks"}).text
+#new_nyt = soup.find('h4',{"class":"svelte-pnqaks"}).text
+
+#covid nyt
+page = requests.get("https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/cases-in-us.html")
+soup = BeautifulSoup(page.content, 'html.parser')
+new = soup.find('span',{"class":"new-cases"}).text[:-11]
 
 #biden
 page = requests.get("https://projects.economist.com/us-2020-forecast/president")
@@ -24,7 +29,7 @@ biden = pct.text[-3:]
 #robinhood
 r.login(username=config.username, password=config.password,expiresIn=2000000)
 port = r.profiles.load_portfolio_profile()
-p_change = (float(port['equity'])-float(port['equity_previous_close']))/float(port['equity_previous_close'])*100
+p_change = (float(port['equity'])-float(port['adjusted_equity_previous_close']))/float(port['adjusted_equity_previous_close'])*100
 
 # send text with Twilio
 client = Client(config.account_sid, config.auth_token)
